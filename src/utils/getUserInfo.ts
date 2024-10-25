2909db226aecb601eb07f7c67d039554fa8775ff
Cédric Sazos <cedric.sazos@tutanota.com>
2024-10-25 18:50:43 +0200

    feat: removed time limit in api

--- src/utils/getUserInfo.ts
+++ src/utils/getUserInfo.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// 2 jours
-const maxDelay: number = 2 * (24 * 3600 * 1000)
-
 export default async function hasUserCompleted(username: string, problemslug: string)
 {
     let q: string = `query getAcSubmissions($username: String!, $limit: Int) {
@@ -17,13 +14,9 @@ export default async function hasUserCompleted(username: string, problemslug: st
     const res = await axios.post("https://leetcode.com/graphql/", 
             {"query": q, "variables": variables, "operationName": "getAcSubmissions" });
     const subs: any[] = res["data"]["data"]["recentAcSubmissionList"];
-    const now = new Date();
     const completed = subs.some((sub) => {
         const slug = sub["titleSlug"];
-        const ts = sub["timestamp"];
-        const completionDate = new Date(parseInt(ts)*1000);
-        const doneInTime = (now.getTime() - completionDate.getTime()) < maxDelay;
-        return slug === problemslug && doneInTime
+        return slug === problemslug
     });
     return completed;
 }
