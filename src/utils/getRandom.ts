ccd14bbad4f143ef90d12151135f3c681bf2422a
Yoni-Deserbaix <yonideserbaix@gmail.com>
2024-10-26 15:17:56 +0200

    fix: custom gifs for y2's discord server

--- src/utils/getRandom.ts
+++ src/utils/getRandom.ts
@@ -1,19 +1,19 @@
 export function getRandomDifficulty(): number {
-    const difficulties: number[] = [1, 2, 3];
-    return difficulties[Math.floor(Math.random() * difficulties.length)];
+  const difficulties: number[] = [1, 2, 3];
+  return difficulties[Math.floor(Math.random() * difficulties.length)];
 }
 
 export function getRandomGif(): string {
-    const gifs: string[] = [
-        'https://media1.tenor.com/m/rz7YXvXEWvIAAAAC/core-keeper-core.gif',
-        'https://media1.tenor.com/m/hmDMrE1yMAkAAAAC/when-the-coding-when-the.gif',
-        'https://media1.tenor.com/m/7pyDepP8SOQAAAAC/cat-wif.gif',
-        'https://media1.tenor.com/m/i3lImBg2UEQAAAAd/scaler-create-impact.gif',
-        'https://media1.tenor.com/m/fq-1gwJhSf8AAAAd/baby-koding-baby.gif',
-        'https://media1.tenor.com/m/XPRG-4ujVMIAAAAd/cat-work-in-progress.gif',
-        'https://media.tenor.com/WWt-bBPOct0AAAAj/cyberkongz-kongz.gif',
-        'https://media1.tenor.com/m/3AQDvhSiPpMAAAAC/dog-hacker.gif',
-        'https://media1.tenor.com/m/y2JXkY1pXkwAAAAC/cat-computer.gif'
-        ];
-    return gifs[Math.floor(Math.random() * gifs.length)];
+  const gifs: string[] = [
+    "https://qph.cf2.quoracdn.net/main-qimg-6ac854a953f07cb8df10b6a6bffc3759",
+    "https://steamuserimages-a.akamaihd.net/ugc/2029473162813738608/986910611937FA421599765516049427E1B8A3C7/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
+    "https://giffiles.alphacoders.com/219/219888.gif",
+    "https://steamuserimages-a.akamaihd.net/ugc/921430008405840800/1A562719B1885AE013F6743BF938F5A029C82817/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
+    "https://steamuserimages-a.akamaihd.net/ugc/816686421609822330/AD91AB6D38E200ADC0423FD93A1B483BDA473FFE/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
+    "https://i.pinimg.com/originals/ab/e3/a0/abe3a048056b29fa9eafca61e4859e73.gif",
+    "https://i.pinimg.com/originals/62/6e/db/626edb1f706ad87f7ec2716e32132f68.gif",
+    "https://steamuserimages-a.akamaihd.net/ugc/579074461706301500/DC7D718B24ADEC70D772EC2D69A98A07150F0F0E/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
+    "https://i.pinimg.com/originals/9e/a7/c3/9ea7c37566fa5d5ec0bb87e6d50a6e20.gif",
+  ];
+  return gifs[Math.floor(Math.random() * gifs.length)];
 }
