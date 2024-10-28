73c48ddfa1ce56d11490cc5723a35f3215c4e422
Alexscdt <alex.schmidt59@icloud.com>
2024-10-28 21:53:04 +0100

    fix: fix link account to ephemeral response

--- src/commands/leetCode/linkAccount.ts
+++ src/commands/leetCode/linkAccount.ts
@@ -23,7 +23,7 @@ module.exports = {
             const discordId = interaction.user.id;
 
             await linkAccount(username, discordId).then(() => {
-                interaction.reply('Votre compte leetCode a bien été lié !');
+                interaction.reply({content: 'Votre compte leetCode a bien été lié !', ephemeral: true});
             }).catch((err) => {
                 log.error("[ERROR - COMMAND - linkAccount] : " + err);
                 interaction.reply('Une erreur est survenue lors de la liaison de votre compte leetCode. Merci de réessayer plus tard.', {ephemeral: true});
