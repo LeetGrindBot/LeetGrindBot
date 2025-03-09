e2f0d45034167816a881d1e9ebbe5edd23d6bcad
Olyxz16 <cedric.sazos@tutanota.com>
2025-03-09 18:58:11 +0100

    feat: balance point system

--- src/utils/computePoints.ts
+++ src/utils/computePoints.ts
@@ -2,13 +2,15 @@ import { getDifficulty, getRate } from "../database/historyProblem";
 
 const mults: { [key: number]: number } = {
     1: 1,
-    2: 2.5,
-    3: 6
+    2: 2,
+    3: 3
 }
 
 export default async function computePoints(problemTitleSlug: string): Promise<number> {
     const difficulty = await getDifficulty(problemTitleSlug);
-    const rate = await getRate(problemTitleSlug);
     const mult = mults[difficulty];
-    return 100 * mult / rate;
+    let rate = await getRate(problemTitleSlug);
+    rate = Math.max(rate, 30);
+    const points = Math.round(100 * mult / rate);
+    return points;
 }
